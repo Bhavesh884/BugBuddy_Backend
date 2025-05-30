31c392cc52315160980108fc3fa3c1614ea506f6
bhavesh884 <bhaveshbhanusalip884@gmail.com>
2025-05-30 12:41:34 +0530

    data sanitization done

--- src/app.js
+++ src/app.js
@@ -4,27 +4,69 @@ const app = express();
 
 const User = require('./models/user');
 
+app.use(express.json());
 
-app.post("/signup", async (req, res) => {
-    const userObj = {
-        profilePic: "xyz.png",
-        firstName: "bhavesh",
-        lastName: "bhanusali",
-        email: "HvH9h@example.com",
-        password: "123456",
-        age: 20,
-        gender: "male",
-        mobileNo: "1234567890"
+app.get("/user", async (req, res) => {
+    const email = req.body.email;
+    try {
+        const users = await User.findOne({ email: email }).exec();
+        console.log(users)
+        if (users) {
+            res.send(users);
+        }
+        else {
+            res.status(404).send("user not found");
+        }
+    } catch (error) {
+        res.status(400).send(error.message);
     }
+})
+app.delete("/user", async (req, res) => {
+    try {
+        const userId = req.body.userId;
+        await User.findByIdAndDelete(userId);
+        res.send("user deleted successfully")
+    } catch (error) {
+        res.status(400).send(error.message);
+    }
+})
+app.patch("/user/:userId", async (req, res) => {
+    const userId = req.params.userId;
+    try {
+        const allowedUpdates = ["firstName", "lastName", "mobileNo", "skills", "description", "profilePic"];
+        const updates = Object.keys(req.body);
+        const isValidOperation = updates.every((update) => allowedUpdates.includes(update));
+        if (!isValidOperation) {
+            // return res.status(400).send({ error: "Invalid updates!" });
+            throw new Error("Invalid updates!");
+        }
+
+        const userObj = req.body;
+        await User.findByIdAndUpdate(userId, userObj, {
+            returnDocument: "after",
+            runValidators: true
+        });
+        res.send("user updated successfully")
+    } catch (error) {
+        res.status(400).send(error.message);
+    }
+})
+app.post("/signup", async (req, res) => {
 
     try {
+        console.log(req.body);
+        const userObj = req.body;
+
         const user = new User(userObj);
         await user.save();
         res.send("user created successfully");
     } catch (error) {
-        res.status(400).send("Error while saving user", error.message);
+        res.status(400).send("Error while saving user " + error);
     }
 });
+app.use("/", () => {
+    console.log("something went wrong");
+})
 
 connectDB().then(() => {
     console.log("connected to database");
--- src/models/user.js
+++ src/models/user.js
@@ -1,36 +1,114 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const userSchema = new mongoose.Schema({
-    profilePic: { type: String }
-
-    , firstName: {
+    profilePic: {
         type: String,
-        required: true
+        trim: true,
+        default: "https://static.everypixel.com/ep-pixabay/0329/8099/0858/84037/3298099085884037069-head.png",
+        validate(value) {
+            if (!validator.isURL(value)) {
+                throw new Error("invalid url" + value)
+            }
+        }
+    },
+    firstName: {
+        type: String,
+        required: true,
+        minlength: 3,
+        maxLength: 20,
+        trim: true
     },
     lastName: {
         type: String,
-        required: true
+        required: true,
+        trim: true,
+        minlength: 3,
+        maxLength: 20
     },
     email: {
         type: String,
-        required: true
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
+        minlength: 3,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error("invalid email" + value)
+            }
+        }
     },
     password: {
         type: String,
-        required: true
+        minlength: 6,
+        required: true,
+        validate(value) {
+            if (value.toLowerCase().includes("password")) {
+                throw new Error("password cannot contain password")
+            }
+
+            if (!validator.isStrongPassword(value)) {
+                throw new Error("password should be strong, it should contain at least one lowercase letter, one uppercase letter, one number and one special character")
+            }
+        }
     },
     age: {
         type: Number,
-        required: true
+        min: 18,
+        required: true,
     },
     gender: {
         type: String,
-        required: true
+        required: true,
+        trim: true,
+        validate(value) {
+            if (!["male", "female", "other"].includes(value)) {
+                throw new Error("invalid gender")
+            }
+
+        }
     },
     mobileNo: {
         type: String,
-        required: true
-    }
+        required: true,
+        unique: true,
+        trim: true,
+        validate(value) {
+            if (value.length !== 10) {
+                throw new Error("invalid mobile number")
+            }
+        },
+
+    },
+    skills: {
+        type: [String],
+        required: true,
+        trim: true,
+        minlength: 1,
+        validate(value) {
+            if (value.length > 12) {
+                throw new Error("too many skills")
+            }
+            // now lowercase every skill and check if we have duplicate skills
+            const lowerCaseSkills = value.map((skill) => skill.toLowerCase());
+            const uniqueSkills = new Set(lowerCaseSkills);
+            if (uniqueSkills.size !== value.length) {
+                throw new Error("duplicate skills")
+            }
+        }
+
+    },
+    description: {
+        type: String,
+        trim: true,
+        default: "I am a developer",
+        minlength: 3
+    },
+}, {
+    timestamps: true,
+    versionKey: false,
+
 });
 const User = mongoose.model("User", userSchema);
 module.exports = User
